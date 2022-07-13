fb90d067b4c3599343ed79dc4c5f3cee05e955ff
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-07-12 22:02:21 -0700

    split components and styled date component

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-export default function ExpenseItems({id, date, title, amount}) {
-
-
+export default function ExpenseItems({ id, date, title, amount }) {
 
   return (
     <div className='expense-item' key={id}>
-      <div>{date.toISOString()}</div>
+        <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>

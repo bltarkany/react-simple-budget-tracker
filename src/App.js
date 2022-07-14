03de13fe9f08b218faea64ce328533d9ae9701fd
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-07-13 21:30:04 -0700

    added item details and css files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import ExpenseItems from './components/ExpenseItems';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>      
+      <h2>Let's get started!</h2>
+      <ExpenseItems />      
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './ExpenseItem.css';
 
-export default function ExpenseItems(){
-    return (
-        <div>
-            <h2>Expense Item</h2>
-        </div>
-    )
+export default function ExpenseItems() {
+
+    // date formatting
+    const expenseDate = new Date(2021, 2, 28);
+
+  return (
+    <div className='expense-item'>
+      <div>July 15, 2022</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$294.67</div>
+      </div>
+    </div>
+  );
 }
